cec8a69b254cca02f29000784df02723539a4699
sonhs99 <sonhs99@gmail.com>
2026-06-12 11:44:51 +0900

    fix: cilium

--- terraform/helm-cilium.tf
+++ terraform/helm-cilium.tf
@@ -3,7 +3,7 @@ resource "helm_release" "cilium" {
   namespace        = "kube-system"
   repository       = "https://helm.cilium.io/"
   chart            = "cilium"
-  version          = "1.20.0"
+  version          = "1.20.0-pre.3"
   create_namespace = false
 
   values = [
